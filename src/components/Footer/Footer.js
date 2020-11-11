66d4797ffb243d303f7eee45ef47d5a37da819ec
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-11-12 00:36:56 +0800

    Footer up to date

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ import {
 	faFacebookF,
     faMediumM,
 } from '@fortawesome/free-brands-svg-icons';
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
 	return (
@@ -30,15 +31,18 @@ const Footer = () => {
                                     <FontAwesomeIcon className="footer-icon" icon={faFacebookF} />
                                 </div>
                             </a>
-                            <a href="https://web.facebook.com/apelmahmudDev/" target="_blank" rel="noopener noreferrer"> 
+                            <a href="https://medium.com/@apelmahmudDev" target="_blank" rel="noopener noreferrer"> 
                                 <div className="icon-items">
                                     <FontAwesomeIcon className="footer-icon" icon={faMediumM} />
                                 </div>
                             </a>
                         </div>
 					</div>
-					<div className="col-md-6">
+					<div className="col-md-6 footer-right-section">
 						<small>All rights reserved | {new Date().getFullYear()} | Created by me</small>
+                        <div className="scrolltop ml-3" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+                            <FontAwesomeIcon className="faAngleUp" icon={faAngleUp} />
+                        </div>
 					</div>
 				</div>
 			</div>
