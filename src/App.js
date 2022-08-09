2ef7b15f19a171a8b08cc4025a75741e63fe2f01
Apel Mahmud <apelmahmud.dev@gmail.com>
2022-08-09 10:55:27 +0600

    feat: redirect main portfolio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Routes from "./components/Routes/Routes";
 
 function App() {
+	useEffect(() => {
+		window.location.href = "https://apelmahmud.vercel.app/";
+	}, []);
+
 	return (
 		<div className="App">
 			<Routes></Routes>
